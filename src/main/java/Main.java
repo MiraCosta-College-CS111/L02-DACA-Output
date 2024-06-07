0f9caf53904a826adcabb782d5f47bdabe5da586
Stephen Cerruti <scerruti@otabi.com>
2024-06-07 06:01:47 +0000

    Initial Version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,39 @@
+// PARTNER NAME:
+// PARTNER NAME:
+// CS111 SECTION #:
+// DATE: 
+
 public class Main
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello world.");
+		//DECLARATION + INITIALIZATION SECTION
+		
+		//INPUT SECTION
+		// N/A
+		
+		//CALCULATION SECTION
+		
+		//OUTPUT SECTION
+		System.out.println("╔══════════════════════════════════════════════════════════════════════");
+		System.out.println("║         UNITED STATES OF AMERICA");
+		System.out.println("║                                  EMPLOYMENT AUTHORIZATION CARD");
+		System.out.println("║                       Surname");
+		System.out.println("║                       ███████ ");
+		System.out.println("║  .----.    .----.     Given Name");
+		System.out.println("║ (   ^  \\  /  ^   )    ███████ ");
+		System.out.println("║        |  |           USCIS#         Category   Card#");
+		System.out.println("║       _/  \\_          ███-███-███    ███        █████████████");
+		System.out.println("║      (_    _)         Country of Birth");
+		System.out.println("║   ,    `--`    ,      ███████ ");
+		System.out.println("║   \\'-.______.-'/      Terms and Conditions");
+		System.out.println("║    \\          /       ███████ ");
+		System.out.println("║     '.--..--.'        Date of Birth   Sex");
+		System.out.println("║       `\"\"\"\"\"`         ██ ███ ████     █");
+		System.out.println("║                       Valid From:     ██/██/██");
+		System.out.println("║                       Card Expires:   ██/██/██");
+		System.out.println("║ ascii art by: jgs     NOT VALID FOR REENTRY TO U.S.");
+		System.out.println("╚══════════════════════════════════════════════════════════════════════");
+		//ascii art credit: http://www.oocities.org/spunk1111/bodypart.htm
 	}
 }
